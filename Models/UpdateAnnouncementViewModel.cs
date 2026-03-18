59965eb7cee267021e9b9c16de349bf73de2e280
Sophia Behei <behey.sofiaa@gmail.com>
2026-03-18 22:58:08 +0200

    removed duplication in forms

--- Models/UpdateAnnouncementViewModel.cs
+++ Models/UpdateAnnouncementViewModel.cs
@@ -1,28 +1,5 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace NoticeBoard_frontend.Models;
 
-public class UpdateAnnouncementViewModel
+public class UpdateAnnouncementViewModel : AnnouncementFormViewModelBase
 {
-    public int Id { get; set; }
-
-    [Display(Name = "Заголовок")]
-    [Required(ErrorMessage = "Заголовок є обов'язковим")]
-    [StringLength(100, MinimumLength = 5, ErrorMessage = "Заголовок має містити від 5 до 100 символів")]
-    public string Title { get; set; } = string.Empty;
-
-    [Display(Name = "Опис")]
-    [Required(ErrorMessage = "Опис є обов'язковим")]
-    [StringLength(1000, MinimumLength = 20, ErrorMessage = "Опис має містити від 20 до 1000 символів")]
-    public string Description { get; set; } = string.Empty;
-
-    [Display(Name = "Категорія")]
-    [Required(ErrorMessage = "Категорія є обов'язковою")]
-    public string Category { get; set; } = string.Empty;
-
-    [Display(Name = "Підкатегорія")]
-    public string? SubCategory { get; set; }
-
-    [Display(Name = "Статус")]
-    public bool Status { get; set; }
 }
